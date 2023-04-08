4842c3ce1907fb8fbe23af17d1f34d03aeceedd9
Twina2 <alicegaspar2012@gmail.com>
2023-04-08 21:45:03 +0100

    World Interactions - Damage Zones and Enemies

--- Assets/Scripts/RubysController.cs
+++ Assets/Scripts/RubysController.cs
@@ -9,8 +9,8 @@ public class RubysController : MonoBehaviour
     public int maxHealth = 5;
     public float timeInvincible = 2.0f;
 
-    int currentHealth;
     public int health { get { return currentHealth; } }
+    int currentHealth;
 
     bool isInvincible;
     float invincibleTimer;
@@ -19,9 +19,15 @@ public class RubysController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
+
         rigidbody2d = GetComponent<Rigidbody2D>();
 
         currentHealth = maxHealth;
@@ -32,6 +38,19 @@ public class RubysController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
 
         if (isInvincible)
         {
@@ -54,6 +73,7 @@ public class RubysController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
 
